1aecb36e510090ef15c5d9b6dd4d575906ab0b3c
Melih Emin Kılıçoğlu <meliheminkilicoglu@Melihs-MacBook-Air.local>
2025-10-24 14:15:57 +0300

    Example 1 completed.

--- ex1.c
+++ ex1.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+// Melih Emin KILICOGLU
+// Firat University
+// C Programming Exercises 01
+// Example 1
+
+int main() {
+    
+    while (1) {
+        // read an integer from the user
+        int input_value;
+        // prompt the user to enter an integer
+        printf("Please enter an integer: ");
+        scanf("%d", &input_value);
+        printf("You entered: %d\n", input_value);
+
+        // check if the entered integer is equal to 3
+
+        if (input_value == 3) {
+            printf("Correct value entered!\n");
+        } else {
+            printf("Incorrect value. Try again pls.\n");
+        }
+    };
+}
