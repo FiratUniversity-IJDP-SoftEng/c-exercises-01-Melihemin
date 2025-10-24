8c1d7c94db117d51c74506e012aa7129d8dbce74
Melih Emin Kılıçoğlu <meliheminkilicoglu@Melihs-MacBook-Air.local>
2025-10-24 14:34:39 +0300

    Example 2 completed.

--- ex2.c
+++ ex2.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+// Melih Emin KILICOGLU
+// Firat University
+// C Programming Exercises 02
+// Example 2
+
+int main() {
+
+
+    int input_age;
+    // get age from user
+    printf("Please enter your age: ");
+    scanf("%d", &input_age);
+
+    // some control and print "Happy Birthday!" input_age times
+    if (input_age > 0) {
+        for (int i = 0; i < input_age; i++) {
+        printf("Happy Birthday!\n");
+    }
+    } else {
+        printf("You tried smth.\n");
+    }
+
+
+}
