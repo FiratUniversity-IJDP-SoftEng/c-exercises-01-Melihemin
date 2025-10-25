733a95809a351272c876704c0703854542d8440d
Melih Emin Kılıçoğlu <meliheminkilicoglu@Melihs-MacBook-Air.local>
2025-10-25 19:16:08 +0300

    Example 5 completed.

--- ex5.c
+++ ex5.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+typedef char* string;
 
+void sayHello(char name[10]);
+
+int main() {
+    char name[10];
+
+    printf("Type your name\n");
+    scanf("%s", name);
+
+    sayHello(name);
+    return 0;
+}
+
+void sayHello(char name[10]) {
+    printf("Hello, %s!\n", name);
+}
