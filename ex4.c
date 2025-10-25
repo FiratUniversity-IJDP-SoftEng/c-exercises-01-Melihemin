2039f0706ddfc34323e02aceb897844e3808c544
Melih Emin Kılıçoğlu <meliheminkilicoglu@Melihs-MacBook-Air.local>
2025-10-25 19:06:51 +0300

    Example 4 completed.

--- ex4.c
+++ ex4.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+    
+    printf("Counting from 2 to 10\n");
+
+    for (int i =2; i <= 10; i+=2) {
+        printf("Count: %d\n", i);
+    }
+    printf("Done");
+    return 0;
+}
